d75bf7df0077a640f2954d3e92849eea27580a11
sam Mireku <samderrty123@gmail.com>
2018-05-07 09:40:17 -0400

    added 3 fields to create function in registercontroller added type to the fillable array in app/user

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -63,8 +63,10 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
+            'name'=> $data ['name'],
             'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+            'password' => bcrypt($data['password']),
+            'type'=> $data ['type'],
         ]);
     }
 }
