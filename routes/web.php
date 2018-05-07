d08e5e2db2883687b86b785ac22f922670e0c2d3
sam Mireku <samderrty123@gmail.com>
2018-05-07 10:15:12 -0400

    made  routes  for each user type.

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,22 @@ Route::delete('/questions/{question_id}/answer/{answer_id}', 'AnswerController@d
 Route::resources([
     'questions' => 'QuestionController',
 ]);
+
+Route::group(['middleware' => 'App\Http\Middleware\AdminMiddleware'], function()
+
+{
+    Route::match(['get', 'post'], '/adminOnlyPage/','HomeController@admin');
+
+});
+Route::group(['middleware' => 'App\Http\Middleware\MemberMiddleware'], function()
+
+{
+    Route::match(['get', 'post'], '/memberOnlyPage/','HomeController@member');
+
+});
+Route::group(['middleware' => 'App\Http\Middleware\SuperAdminMiddleware'], function()
+
+{
+    Route::match(['get', 'post'], '/superAdminOnlyPage/','HomeController@super_admin');
+
+});  
